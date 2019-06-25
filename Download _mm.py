3aeda440e76cbf0154275559df6e72c99e51c683
3194libin <15764747737@qq.com>
2019-06-25 17:13:16 +0800

    批量下载图片

--- Download _mm.py
+++ Download _mm.py
@@ -6,6 +6,7 @@ def url_open(url):
     req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36')
     response = urllib.request.urlopen(url)
     html = response.read()
+
     return html
 
 def get_page(url):
@@ -25,27 +26,31 @@ def find_imgs(url):
     while a != -1:
         b = html.find('.jpg',a,a+255)
         if b != -1:
-         img_addrs.append(html[a+9,b+4])
+            img_addrs.append(html[a+9:b+4])
         else:
-         b = a+9
+            b = a+9
         a = html.find('img src=',b)
+    return img_addrs
+
+def save_imgs(folder,img_addrs):
     for each in img_addrs:
-        print("each")
-def save_img(folder,img_addrs):
-    pass
+        filename = each.split('/')[-1]
+        with open(filename,'wb') as f:
+            img = url_open(each)
+            f.write(img)
 
 def Download_mm(folder = 'OOXX',pages = 10):
     os.mkdir(folder)
     os.chdir(folder)
 
     url = 'http://jandan.net/ooxx/'
-    page_number = int(get_page(url))
+    page_num = int(get_page(url))
 
     for i in range(pages):
-        page_number -= i
-        page_url = url + 'page-' + str(page_number) + 'comments'
+        page_num -= i
+        page_url = url + 'page-' + str(page_num) + 'comments'
         img_addrs = find_imgs(page_url)
-        save_img(folder,img_addrs)
+        save_imgs(folder,img_addrs)
 
 if __name__ == '__main__':
     Download_mm()
